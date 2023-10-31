32a25820ba20bef3c68f63b20c3f213869fd223f
Kenny Chen <kennygchen@yahoo.com>
2023-10-30 22:54:21 -0700

    overlay info added

--- web/src/components/tabs/CallForceGraph.vue
+++ web/src/components/tabs/CallForceGraph.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="forceGraphHolder" id="forceGraphHolder"></div>
+  <div>
+    <div class="forceGraphHolder" id="forceGraphHolder"></div>
+    <OverlayInfo class="overlay-container" :clickedObject="this.clickedObject" />
+  </div>
 </template>
 
 <script>
 import ForceGraph3D from '3d-force-graph';
 import gdot from "./gdot.json";
 import * as THREE from "three";
+import OverlayInfo from './OverlayInfo.vue';
 
 export default {
   name: 'CallForceGrpah',
+  components: {
+    OverlayInfo: () => import('./OverlayInfo.vue'),
+  },
   data() {
     return {
-      graph: null
+      graph: null,
+      clickedObject: {
+        link: null,
+        node: null
+      }
     }
   },
   mounted() {
@@ -56,6 +67,7 @@ export default {
 
       this.graph = ForceGraph3D()
         (document.getElementById("forceGraphHolder"))
+        .backgroundColor("#010011")
         .graphData(gData)
         .width(width)
         .height(height)
@@ -119,6 +131,7 @@ export default {
         )
         .linkSource("source")
         .linkTarget("target")
+        .linkOpacity(0.25)
         .linkDirectionalParticles(link => highlightLinks.has(link) ? 4 : 0)
         .linkDirectionalParticleWidth(2)
         .linkDirectionalParticleSpeed(0.005)
@@ -149,17 +162,42 @@ export default {
           }
 
           this.rerenderGraph();
+        })
+        .onNodeClick(node => {
+          this.clickedObject.node = node
+          // console.log(this.clickedObject.node)
         });
     },
     rerenderGraph() {
-      console.log("rerender") +
-        this.graph
-          .linkWidth(this.graph.linkWidth())
-          .linkDirectionalParticles(this.graph.linkDirectionalParticles())
-          .nodeThreeObject(this.graph.nodeThreeObject());
+      this.graph
+        .linkWidth(this.graph.linkWidth())
+        .linkDirectionalParticles(this.graph.linkDirectionalParticles())
+        .nodeThreeObject(this.graph.nodeThreeObject());
     },
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.overlay-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 550px;
+  height: auto;
+  border-radius: 25px;
+  margin: 10px;
+  padding: 15px;
+  background: black;
+  border: 1px solid white;
+  color: white;
+  opacity: 85%;
+  z-index: 5;
+}
+</style>
---
+++ web/src/components/tabs/OverlayInfo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div>Left-click: view node/link info</div>
+        {{ clickedObject.node.key }}
+        <div>Reft-click: focus on node</div>
+    </div>
+    <!-- <div v-if="">
+            <div>Key: {clicked.clicked ? clicked.clicked.key : "null"}</div>
+        <div>Source: {clicked.clicked ? clicked.clicked.source.key : "null"}</div>
+        <div>Target: {clicked.clicked ? clicked.clicked.target.key : "null"}</div>
+        <div>Weight: {clicked.clicked ? clicked.clicked.attributes.weight : "null"}</div>
+    </div>
+    <div v-if="">
+            <div>Key: {clicked.clicked ? clicked.clicked.key : "null"}</div>
+        <div>Modularity class: {clicked.clicked ? clicked.clicked.attributes.modularity_class : "null"}</div>
+        <div>Memory Object: {clicked.clicked ? clicked.clicked.attributes.MemoryObject : "null"}</div>
+        <div>Offset: {clicked.clicked ? clicked.clicked.attributes.Offset : "null"}</div>
+    </div> -->
+</template>
+
+<script>
+export default {
+    name: 'OverlayInfo',
+    props: ['clickedObject'],
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+            console.log("overlay info")
+            console.log(clickedObject)
+        }
+    }
+}
+</script>
+
+<style></style>
